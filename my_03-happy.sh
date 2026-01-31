d0ae50113346496055d10eaa053b93710d3db4e3
aeristran <thmy263@gmail.com>
2026-01-31 02:57:58 -0500

    Added and changed file

---
+++ my_03-happy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "You are happy?"
+read answer
+
+if [ "$answer" = "yes" ]; then
+   echo "Smile :)"
+else
+   echo "Still Smile :)"
+fi
+
+# here are the other string comparison operators
+# != , -n (not an empty string) , -z (an empty string)
+
+# exercise: write a script that prints whether today is
+# the weekend or not
+echo "Which day of a week is today?"
+read day
+if [[ ${day,,} == "sunday" || ${day,,} == "sunday" ]]
+then 
+   echo "Horayyyy!! Today is the weekend. Enjoy yourself! Be ready for the next week."
+else 
+   echo "Today is a weekday. Keep focus on your work."
+fi
